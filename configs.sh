2dd849e589d0e59ca1b6080b6469ce67c6cc40d3
Reeshav Mainali <reeshav.mainali1990@gmail.com>
2023-08-03 09:01:01 +0545

    lala

--- configs.sh
+++ configs.sh
@@ -1,8 +1,5 @@
 #!/bin/bash
 
-#run with sudo privalage
-
-
 #copying configs 
 cp -r ./configs/* ~/.config/
 
@@ -11,9 +8,6 @@ chsh -s /bin/fish
 
 #set wallpaper
 cp -r ./Wallpapers/ ~/Pictures/
-
-#enable display manager and bluetooth
-sudo systemctl enable sddm bluetooth auto-cpufreq
 
 #start Hyprland
 Hyprland
